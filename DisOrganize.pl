6384b064b2cd6af955f0648435af2d65010f16ee
Terry Nightingale <tnight@pobox.com>
2016-04-14 17:20:05 -0700

    Added license and readme information to "product-ize" the DisOrganize program.

--- DisOrganize.pl
+++ DisOrganize.pl
@@ -1,7 +1,26 @@
 #!/usr/bin/perl -nw
 #
+# (c)Copyright, Terry Nightingale 2015-2016
+#
 # usage: DisOrganize.pl iTunes\ Library.xml
 #
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, version 2 only.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Visit this URL for more information:
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+#
+############################################################################
 
 use strict;
 use File::Spec;
